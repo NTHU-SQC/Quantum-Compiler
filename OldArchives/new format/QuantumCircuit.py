6a9ec8664117f69f5684a4c8034862e9c28973dc
technidra <avenger1331@ymail.com>
2021-09-10 19:14:44 +0800

    new folder

---
+++ OldArchives/new format/QuantumCircuit.py
@@ -0,0 +1,275 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Sep  8 19:34:12 2021
+
+@author: Alaster
+"""
+
+import numpy as np
+import pickle
+from WaveModule import QubitChannel
+
+
+class QuantumCircuit(object):
+
+    def __init__(self, qubit={}, blockNum=1, readout={}):
+        """
+        Create a timeline of quantum circuit diagram.
+
+        Parameters
+        ----------
+        qubit : dict/list, optional
+            Qubit control channel assignment. For dict type the argument uses
+            {qubit name : qubit index, ...} format, while for list type is
+            [qubit name, ...] as the indices have the same order as the list.
+            The default is {}.
+        blockNum : int, optional
+            Number of time indices. The default is 1.
+        readout : dict/list, optional
+            Similar to qubit for special readout purposes. The index order can
+            be mixed with qubit in dict mode while the in list mode the index
+            order is always later than qubit ones. The default is {}.
+
+        """
+        self.diagram = np.asarray(
+            [[np.nan] * blockNum] * (len(qubit) + len(readout)),
+            dtype=object
+            )
+        # check datatype and assign
+        if isinstance(qubit, list):
+            self._qubitDict = dict(zip(qubit, range(len(qubit))))
+        elif isinstance(qubit, dict):
+            self._qubitDict = qubit
+        else:
+            raise TypeError('qubitDict: Unsupported format')
+        if isinstance(readout, list):
+            self._readoutDict = dict(
+                zip(readout, np.arange(len(readout)) + len(qubit))
+                )
+        elif isinstance(readout, dict):
+            self._readoutDict = readout
+        else:
+            raise TypeError('readoutDict: Unsupported format')
+        # index check
+        for key, val in {**self._qubitDict, **self._readoutDict}.items():
+            if val > len(self.diagram[:, 0]):
+                raise ValueError(
+                    'Found ' + str(key) + ' out of bound: ' + str(val)
+                    )
+        self._name = ''
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, name=''):
+        self._name = name
+
+    @property
+    def qubitDict(self):
+        return self._qubitDict
+
+    @qubitDict.setter
+    def qubitDict(self, qubit={}):
+        if len(self._qubitDict) != len(qubit):
+            raise ValueError('Name field size mismatched')
+        self._qubitDict = qubit
+
+    @property
+    def readoutDict(self):
+        return self._readoutDict
+
+    @readoutDict.setter
+    def readoutDict(self, readout={}):
+        if len(self._readoutDict) != len(readout):
+            raise ValueError('Name field size mismatched')
+        self._readoutDict = readout
+
+    def assign(self, gateObj, mapping):
+        """
+        Assign the Gate object to the specified index.
+
+        Parameters
+        ----------
+        gateObj : Gate or QubitChannel
+            Gate or QubitChannel object.
+        mapping : dict or list
+            The datatype determines how the gateObj is assigned:
+                dict -> name-index pair for Gate object assignment. The 'name'
+                    key is the name of the QubitChannel object and the 'index'
+                    value is the corresponding indices in the circuit diagram.
+                list -> indices for single QubitChannel object assignment.
+
+        Returns
+        -------
+        None.
+
+        """
+        if isinstance(mapping, dict):
+            for key, idx_tag in mapping.items():
+                qubitIdx = self.get_index(idx_tag[0])
+                blockNum = idx_tag[1] - len(self.diagram[0, :]) + 1
+                if blockNum > 0:
+                    self.diagram = np.concatenate((
+                        self.diagram, np.asarray(
+                            [[np.nan] * blockNum] * len(self.diagram[:, 0])
+                            )
+                        ), axis=1)
+                self.diagram[qubitIdx, idx_tag[1]] = gateObj._qubitDict[key]
+        else:
+            qubitIdx = self.get_index(mapping[0])
+            blockNum = mapping[1] - len(self.diagram[0, :]) + 1
+            if blockNum > 0:
+                self.diagram = np.concatenate((
+                    self.diagram, np.asarray(
+                        [[np.nan] * blockNum] * len(self.diagram[:, 0])
+                        )
+                    ), axis=1)
+            self.diagram[qubitIdx, mapping[1]] = gateObj
+
+    def get_index(self, qubit_name):
+        """
+        Get the index of the qubit/readout according to its name.
+
+        Parameters
+        ----------
+        qubit_name : str, int
+            Name of the qubit. Return the input if the input is an int.
+
+        Returns
+        -------
+        int
+            Qubit index.
+
+        """
+        if isinstance(qubit_name, int):
+            return qubit_name
+        try:
+            return self.qubitDict[qubit_name]
+        except KeyError:
+            return self.readoutDict[qubit_name]
+
+    def compileCkt(self):
+        """
+        Compile the quantum circuit.
+
+        """
+        f = np.vectorize(lambda x: isinstance(x, QubitChannel))
+        table = f(self.diagram)
+        col_bool = np.bitwise_or.reduce(table, axis=1)
+        # filter nan in 'qubit' direction
+        if not np.bitwise_and.reduce(col_bool):
+            raise ValueError('Found unassigned qubit')
+        # filter nan in 'time' direction
+        row_bool = np.bitwise_or.reduce(table, axis=0)
+        diagram = self.diagram[:, row_bool]
+        table = table[:, row_bool]
+        # align QubitChannel objects in the table column by column
+        for time_idx in range(len(table[0, :])):
+            diagram[table[:, time_idx], time_idx
+                    ] = QubitChannel.alignQubitChannels(
+                        *diagram[table[:, time_idx], time_idx]
+                        )
+        # replace nans with null QubitChannel objects
+        for qubit_idx in range(len(diagram[:, 0])):
+            for time_idx in range(len(diagram[0, :])):
+                if table[qubit_idx, time_idx]:
+                    continue
+                span_idx = np.where(f(diagram[:, time_idx]))[0][0]
+                wire_idx = np.where(f(diagram[qubit_idx, :]))[0][0]
+                diagram[qubit_idx, time_idx] = QubitChannel.null(
+                    diagram[span_idx, time_idx], diagram[qubit_idx, wire_idx]
+                    )
+        self.compiled = np.sum(diagram, axis=1)
+
+    def __matmul__(self, qubit):
+        """
+        Return the compiled QubitChannel amplitude data y.
+
+        Parameters
+        ----------
+        qubit : str, int
+            Index or the name of qubit.
+
+        Returns
+        -------
+        list
+            List of y from each wire in the compiled QubitChannel object.
+
+        """
+        if isinstance(qubit, str):
+            qubit = self.get_index(qubit)
+        return self.compiled[qubit].y
+
+    @classmethod
+    def save(cls, *args):
+        """
+        Save QuantumCircuit object to .qckt files.
+
+        Parameters
+        ----------
+        cls : QuantumCircuit class
+            QuantumCircuit class.
+        *args : QuantumCircuit
+            Object to be saved.
+
+        """
+        for i, gwObj in enumerate(args):
+            if gwObj.name == '':
+                gwObj.name = input(
+                    'Empty name string for {i}th item, set object name:'
+                    )
+            with open(f'{gwObj.name}.qckt', 'wb') as f:
+                pickle.dump(gwObj, f, pickle.HIGHEST_PROTOCOL)
+
+    @classmethod
+    def load(cls, *args):
+        """
+        Load QuantumCircuit object from .qckt files.
+
+        Parameters
+        ----------
+        cls : QuantumCircuit class
+            QuantumCircuit class.
+        *args : String
+            Filename.
+
+        Returns
+        -------
+        QuantumCircuit
+            Loaded objects.
+
+        """
+        objList = []
+        for filename in args:
+            with open(filename, 'rb') as f:
+                objList += [pickle.load(f)]
+        return *objList,
+
+
+if __name__ == '__main__':
+    from shape_functionV4 import gaussian, get_x
+    from WaveModule import Wave, Waveform
+    from TemplateModule import GenericGate
+    a = Wave(gaussian, [get_x(10e-6), 5e-6, 1e-6])
+    b = Waveform(Waveform._nullBlock(a.span*2))
+    b1 = ~(b+b+~a)
+    b2 = ~(~a+b+~a+b)
+    c = ~b / ~a
+    c.name = 'c'
+    # kk = QuantumCircuit({'a': 0, 'b': 1, 'c': 2}, 10, ['readout'])
+    kk = QuantumCircuit(['a', 'b', 'c'], 10, ['readout'])
+    kk.assign(c, (0, 0))
+    kk.assign(c, (1, 0))
+    z = GenericGate(c)
+    kk.assign(z, {'c': (0, 11)})
+    kk.assign(b1, (0, 5))
+    kk.assign(b2, (2, 5))
+    kk.assign(z, {'c': ('readout', 10)})
+    kk.assign(z, {'c': ('c', 9)})
+    kk.assign(z, {'c': ('c', 8)})
+
+    kk.compileCkt()
+    print(kk@'c')
+    pass
---
+++ OldArchives/old format/shape_functionV4.py
@@ -0,0 +1,398 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Apr 10 18:09:39 2021
+
+@author: Alaster
+
+Create the following functions to build waveform
+"""
+
+import numpy as np
+
+
+def get_x(span=.0, sampling_rate=1e9, delEnd=True):
+    """
+    Formatted timeline creation.
+
+    Parameters
+    ----------
+    span : float, optional
+        Overall length of timeline. The default is .0.
+    sampling_rate : float, optional
+        Sampling rate for DAC. The default is 1e9 (Suggested).
+    delEnd : boolean, optional
+        Delete the last point. The default is True.
+
+    Returns
+    -------
+    numpy.array
+
+    """
+    points = int(round(span*sampling_rate, 3))+1
+    x = np.linspace(0, points-1, points)/sampling_rate
+    end = None
+    if delEnd:
+        end = -1
+    return x[:end]
+
+
+def gaussian(x, peak_x=.0, sigma=1.0, name=''):
+    """
+    Gaussian pulse generating function
+
+    Parameters
+    ----------
+    x : numpy.array
+        Wave event timeline, start from 0 is demanded.
+    peak_x : float, optional
+        Position of Gaussian pulse peak. The default is .0.
+    sigma : float, optional
+        Standard deviation. The default is 1.0.
+    name : string, optional
+        Name of the wave object. The default is ''.
+
+    Returns
+    -------
+    properties : dict
+        A dictionary of relavent data to be encapsulated.
+
+    """
+    x = x.astype(np.float)
+    variables = {'function': gaussian, 'peak_x': peak_x, 'sigma': sigma}
+    y = np.exp((-1 * (x - peak_x)**2) / (2 * sigma**2))
+    return packer(x, y, variables, name=name)
+
+
+def const(x, setOne=True, name=''):
+    """
+    Constant level generating function
+
+    Parameters
+    ----------
+    x : numpy.array
+        Wave event timeline, start from 0 is demanded.
+    setOne : boolean, optional
+        Toggle True(False) to output 1s(0s). The default is True.
+    name : string, optional
+        Name of the wave object. The default is ''.
+
+    Returns
+    -------
+    properties : dict
+        A dictionary of relavent data to be encapsulated.
+
+    """
+    x = x.astype(np.float)
+    variables = {'function': const, 'setOne': setOne}
+    offset = 0
+    if setOne:
+        offset += 1
+    y = np.zeros(x.size) + offset
+    return packer(x, y, variables, name=name)
+
+
+def exp_rising(x, peak_x=0.0, tau=1.0, name=''):
+    """
+    Exponential rising pulse generating function
+
+    Parameters
+    ----------
+    x : numpy.array
+        Wave event timeline, start from 0 is demanded.
+    peak_x : float, optional
+        Position of pulse peak. The default is 0.0.
+    tau : float, optional
+        Characteristic time for exponential pulse. The default is 1.0.
+    name : string, optional
+        Name of the wave object. The default is ''.
+
+    Returns
+    -------
+    properties : dict
+        A dictionary of relavent data to be encapsulated.
+
+    """
+    x = x.astype(np.float)
+    variables = {'function': exp_rising, 'peak_x': peak_x, 'tau': tau}
+    y = np.concatenate((
+        np.exp((x[x <= peak_x] - peak_x) / tau), np.zeros(x[x > peak_x].size)
+        ))
+    return packer(x, y, variables, name=name)
+
+
+def exp_falling(x, peak_x=0.0, tau=1.0, name=''):
+    """
+    Exponential falling pulse generating function
+
+    Parameters
+    ----------
+    x : numpy.array
+        Wave event timeline, start from 0 is demanded.
+    peak_x : float, optional
+        Position of pulse peak. The default is 0.0.
+    tau : float, optional
+        Characteristic time for exponential pulse. The default is 1.0.
+    name : string, optional
+        Name of the wave object. The default is ''.
+
+    Returns
+    -------
+    properties : dict
+        A dictionary of relavent data to be encapsulated.
+
+    """
+    x = x.astype(np.float)
+    variables = {'function': exp_falling, 'peak_x': peak_x, 'tau': tau}
+    y = np.concatenate((
+        np.zeros(x[x < peak_x].size), np.exp((peak_x - x[x >= peak_x]) / tau)
+        ))
+    return packer(x, y, variables, name=name)
+
+
+def gaussian_square(x, first_peak_x=5.0, flat=10.0, sigma=1.0, name=''):
+    """
+    Gaussian square pulse generating function
+
+    Parameters
+    ----------
+    x : numpy.array
+        Wave event timeline, start from 0 is demanded.
+    first_peak_x : float, optional
+        Position of the first Gaussian edge peak. The default is 5.0.
+    flat : float, optional
+        Time span of flat top (1s). The default is 10.0.
+    sigma : float, optional
+        Standard deviation of Gaussian edge. The default is 1.0.
+    name : string, optional
+        Name of the wave object. The default is ''.
+
+    Returns
+    -------
+    properties : dict
+        A dictionary of relavent data to be encapsulated.
+
+    """
+    x = x.astype(np.float)
+    variables = {'function': gaussian_square, 'first_peak_x': first_peak_x,
+                 'flat': flat, 'sigma': sigma}
+    y = np.concatenate((
+        gaussian(x[x <= first_peak_x], first_peak_x, sigma)['y'],
+        np.ones(
+            x[np.logical_and(x > first_peak_x, x <= first_peak_x + flat)].size
+            ),
+        gaussian(
+            x[x > first_peak_x + flat], x[x > first_peak_x + flat][0], sigma
+            )['y']
+        ))
+    return packer(x, y, variables, name=name)
+
+
+def exp_square(x, first_peak_x=5.0, flat=10.0, tau=1.0, name=''):
+    """
+    Generating function for square pulse with exponential edges.
+
+    Parameters
+    ----------
+    x : numpy.array
+        Wave event timeline, start from 0 is demanded.
+    first_peak_x : float, optional
+        Position of the first Gaussian edge peak. The default is 5.0.
+    flat : float, optional
+        Time span of flat top (1s). The default is 10.0.
+    tau : float, optional
+        Characteristic time of exponential edge. The default is 1.0.
+    name : string, optional
+        Name of the wave object. The default is ''.
+
+    Returns
+    -------
+    properties : dict
+        A dictionary of relavent data to be encapsulated.
+
+    """
+    x = x.astype(np.float)
+    variables = {'function': gaussian_square, 'first_peak_x': first_peak_x,
+                 'flat': flat, 'tau': tau}
+    y = np.concatenate((
+        exp_rising(x[x <= first_peak_x], first_peak_x, tau)['y'],
+        np.ones(
+            x[np.logical_and(x > first_peak_x, x <= first_peak_x + flat)].size
+            ),
+        exp_falling(
+            x[x > first_peak_x + flat], x[x > first_peak_x + flat][0], tau
+            )['y']
+        ))
+    return packer(x, y, variables, name=name)
+
+
+def sine(x, period=10.0, start_phase=0.0, name=''):
+    """
+    Sine pulse generating function
+
+    Parameters
+    ----------
+    x : numpy.array
+        Wave event timeline, start from 0 is demanded.
+    period : float, optional
+        Wave period. The default is 10.0.
+    start_phase : float, optional
+        Starting phase in radian. The default is 0.0.
+    name : string, optional
+        Name of the wave object. The default is ''.
+
+    Returns
+    -------
+    properties : dict
+        A dictionary of relavent data to be encapsulated.
+
+    """
+    x = x.astype(np.float)
+    variables = {
+            'function': sine, 'period': period, 'start_phase': start_phase}
+    y = np.sin(2*np.pi*(x / period) + start_phase)
+    return packer(x, y, variables, name=name)
+
+
+def sine2(x, frequency=10.0, start_phase=0.0, name=''):
+    """
+    Sine pulse generating function
+
+    Parameters
+    ----------
+    x : numpy.array
+        Wave event timeline, start from 0 is demanded.
+    frequency : float, optional
+        Wave frequency. The default is 10.0.
+    start_phase : float, optional
+        Starting phase in radian. The default is 0.0.
+    name : string, optional
+        Name of the wave object. The default is ''.
+
+    Returns
+    -------
+    properties : dict
+        A dictionary of relavent data to be encapsulated.
+
+    """
+    x = x.astype(np.float)
+    variables = {
+            'function': sine,
+            'frequency': frequency,
+            'start_phase': start_phase
+            }
+    y = np.sin(2 * np.pi * frequency * x + start_phase)
+    return packer(x, y, variables, name=name)
+
+
+def cosine(x, period=10.0, start_phase=0.0, name=''):
+    """
+    Cosine pulse generating function
+
+    Parameters
+    ----------
+    x : numpy.array
+        Wave event timeline, start from 0 is demanded.
+    period : float, optional
+        Wave period. The default is 10.0.
+    start_phase : float, optional
+        Starting phase in radian. The default is 0.0.
+    name : string, optional
+        Name of the wave object. The default is ''.
+
+    Returns
+    -------
+    properties : dict
+        A dictionary of relavent data to be encapsulated.
+
+    """
+    return sine(x, period, start_phase + np.pi/2, name)
+
+
+def cosine2(x, frequency=10.0, start_phase=0.0, name=''):
+    """
+    Cosine pulse generating function
+
+    Parameters
+    ----------
+    x : numpy.array
+        Wave event timeline, start from 0 is demanded.
+    frequency : float, optional
+        Wave frequency. The default is 10.0.
+    start_phase : float, optional
+        Starting phase in radian. The default is 0.0.
+    name : string, optional
+        Name of the wave object. The default is ''.
+
+    Returns
+    -------
+    properties : dict
+        A dictionary of relavent data to be encapsulated.
+
+    """
+    return sine2(x, frequency, start_phase + np.pi/2, name)
+
+
+def square(x, start=.0, flat=1.0, name=''):
+    """
+    Square pulse generating function
+
+    Parameters
+    ----------
+    x : numpy.array
+        Wave event timeline, start from 0 is demanded.
+    start : float, optional
+        Start time for 1s. The default is .0.
+    flat : float, optional
+        Time span for 1s. The default is 1.0.
+    name : string, optional
+        Name of the wave object. The default is ''.
+
+    Returns
+    -------
+    properties : dict
+        A dictionary of relavent data to be encapsulated.
+
+    """
+    x = x.astype(np.float)
+    variables = {'function': square, 'start': start, 'flat': flat}
+    y = np.concatenate((
+        np.zeros(x[x < start].size),
+        np.ones(x[np.logical_and(x >= start, x <= start + flat)].size),
+        np.zeros(x[x > start + flat].size)
+        ))
+    return packer(x, y, variables, name=name)
+
+
+def packer(x, y, variables={}, appendRule=[True, True], name=''):
+    """
+    Given x, y ,create a corresponding dictionary package.
+
+    Parameters
+    ----------
+    x : numpy.array
+        Wave event timeline, start from 0 is demanded.
+    y : numpy.array
+        Amplitude data array.
+    variables : dict, optional
+        Some information about the wave.
+    name : string, optional
+        Name of the wave object. The default is ''.
+
+    Returns
+    -------
+    properties : dict
+        A dictionary of relavent data to be encapsulated.
+
+    """
+    properties = {
+            'name': name,
+            'variables': variables,
+            'x': x,
+            'y': y,
+            'appendRule': appendRule
+            }
+    return properties
+
+
+if __name__ == '__main__':
+    a = get_x(10e-06)
